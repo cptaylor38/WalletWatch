173880ac47e255d7a2bd16b7222a605c5385c122
Corbin Taylor <cptaylor38@yahoo.com>
2019-09-13 18:03:12 -0400

    testing

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
-const path = require('path');
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const app = express();
 const PORT = process.env.PORT || 5000;
 const passport = require('passport');
 const FacebookStrategy = require("passport-facebook").Strategy;
 const keys = require('./config');
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+let user = {};
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
-
-}
-app.use("/", routes);
-app.use(cors());
-app.use(passport.initialize());
 
 passport.serializeUser((user, cb) => { cb(null, user) });
 passport.deserializeUser((user, cb) => { cb(null, user) });
@@ -31,6 +20,7 @@ passport.use(new FacebookStrategy({
     callbackURL: "/auth/facebook/callback"
 },
     (accessToken, refreshToken, profile, cb) => {
+        user = { ...profile };
         return cb(null, profile);
     }));
 
@@ -40,9 +30,13 @@ passport.use(new GoogleStrategy({
     callbackURL: "/auth/google/callback"
 },
     (accessToken, refreshToken, profile, cb) => {
-        console.log(cb);
+        user = { ...profile };
         return cb(null, profile);
     }));
+
+const app = express();
+app.use(cors());
+app.use(passport.initialize());
 
 app.get("/auth/facebook", passport.authenticate("facebook"));
 app.get("/auth/facebook/callback",
@@ -60,6 +54,20 @@ app.get("/auth/google/callback",
     (req, res) => {
         res.redirect("/profile");
     });
+
+app.get("/user", (req, res) => {
+    res.send(user);
+})
+
+app.get("/auth/logout", (req, res) => {
+    res.redirect("/");
+})
+
+app.use("/", routes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WalletWatchDb");
 
