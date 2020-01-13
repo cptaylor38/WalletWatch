ccf73aa05e64320a14efd92680d38f59f1834ecf
Corbin Taylor <cptaylor38@yahoo.com>
2020-01-12 19:19:46 -0500

    merging dependabot fixes

--- client/src/components/Content/Content.jsx
+++ client/src/components/Content/Content.jsx
@@ -7,127 +7,164 @@ import HelperText from './HelperText/HelperText';
 import './Content.css';
 import ExpenseForm from '../ExpenseForm/ExpenseForm';
 import ChargeItem from './ChargeItem/ChargeItem';
-import API from "../../clientRoutes/API";
+import API from '../../clientRoutes/API';
 import moment from 'moment';
 
 const Content = ({ display, user }) => {
-    const [content, setContent] = useState(null);
-    const [monthlyCharges, setMonthlyCharges] = useState(null);
-    const [oneTime, setOneTime] = useState(null);
-    const [retrieve, setRetrieve] = useState(false);
-    const [expenseToggle, setExpenseToggle] = useState(false);
-    const [totalMonthly, setTotalMonthly] = useState(null);
+  const [content, setContent] = useState(null);
+  const [monthlyCharges, setMonthlyCharges] = useState(null);
+  const [oneTime, setOneTime] = useState(null);
+  const [retrieve, setRetrieve] = useState(false);
+  const [expenseToggle, setExpenseToggle] = useState(false);
+  const [totalMonthly, setTotalMonthly] = useState(null);
 
-    const retrieveContent = () => {
-        if (display !== null && user !== null) {
-            API.getCategoryData({ id: user, category: display })
-                .then(response => { setContent(response.data); setRetrieve(false); });
-
-        }
+  const retrieveContent = () => {
+    if (display !== null && user !== null) {
+      API.getCategoryData({ id: user, category: display }).then(response => {
+        setContent(response.data);
+        setRetrieve(false);
+      });
     }
+  };
 
-    const sortExpenses = () => {
-        if (content !== null && display !== 'home') {
-            let sortedOneTime = content.filter(item => item.monthly === false);
-            let sortedMonthly = content.filter(item => item.monthly === true);
-            let datedOneTime = sortRecent(sortedOneTime);
-            let datedMonthly = sortRecent(sortedMonthly);
-            setMonthlyCharges(datedMonthly);
-            setOneTime(datedOneTime);
-        }
+  const sortExpenses = () => {
+    if (content !== null && display !== 'home') {
+      let sortedOneTime = content.filter(item => item.monthly === false);
+      let sortedMonthly = content.filter(item => item.monthly === true);
+      let datedOneTime = sortRecent(sortedOneTime);
+      let datedMonthly = sortRecent(sortedMonthly);
+      setMonthlyCharges(datedMonthly);
+      setOneTime(datedOneTime);
     }
+  };
 
-    const sortRecent = array => {
-        return array.sort((a, b) => moment(b.date).unix() - moment(a.date).unix())
+  const sortRecent = array => {
+    return array.sort((a, b) => moment(b.date).unix() - moment(a.date).unix());
+  };
+
+  const expenseInputToggle = () => {
+    setExpenseToggle(!expenseToggle);
+  };
+
+  useEffect(() => {
+    let expenseDiv = document.querySelector('#expenseInputPaper');
+    if (expenseToggle === false) {
+      expenseDiv.style.display = 'none';
     }
-
-    const expenseInputToggle = () => {
-        setExpenseToggle(!expenseToggle);
+    if (expenseToggle === true) {
+      expenseDiv.style.display = 'flex';
     }
+  }, [expenseToggle]);
 
-    useEffect(() => {
-        let expenseDiv = document.querySelector('#expenseInputPaper');
-        if (expenseToggle === false) {
-            expenseDiv.style.display = 'none'
-        }
-        if (expenseToggle === true) {
-            expenseDiv.style.display = 'flex'
-        }
-    }, [expenseToggle]);
+  useEffect(() => {
+    if (monthlyCharges !== null && monthlyCharges.length > 0) {
+      let total = 0;
+      monthlyCharges.map(item => (total += item.amount));
+      setTotalMonthly(total);
+    }
+  }, [monthlyCharges]);
 
-    useEffect(() => {
-        if (monthlyCharges !== null && monthlyCharges.length > 0) {
-            let total = 0;
-            monthlyCharges.map(item => total += item.amount);
-            setTotalMonthly(total);
-        }
-    }, [monthlyCharges])
+  useEffect(retrieveContent, [display, user, retrieve]);
+  useEffect(sortExpenses, [content]);
 
-    useEffect(retrieveContent, [display, user, retrieve]);
-    useEffect(sortExpenses, [content]);
-
-
-
-    return (
+  return (
+    <>
+      {display === 'home' ? (
         <>
-            {display === 'home' ?
-                <>
-                    <Grid container id='addCont'>
-                        <Grid item xs={12} id='addItem'>
-                            <Paper id='addPaper'>
-                                <Grid item xs={12} id='addExpenseSection'>
-                                    <Button id='toggleExpenseBtn' onClick={expenseInputToggle}><p style={{ marginRight: 10 }}>Add Expense</p> <FaFolderPlus style={{ fontSize: 24 }} /> </Button>
-                                </Grid>
-                            </Paper>
-                            <Paper id='expenseInputPaper'>
-                                <Grid item xs={12} id='expenseInputSection'>
-                                    {user ? <ExpenseForm user={user} update={() => retrieveContent()} setRetrieve={setRetrieve} /> : null}
-                                </Grid>
-                            </Paper>
-                        </Grid>
-                    </Grid>
-                    <Grid container id='homeContentGrid'>
-                        <Overview user={user} />
-                    </Grid>
-                </>
-                :
-                <>
-                    <Grid container id='addCont'>
-                        <Grid item xs={12} id='addItem'>
-                            <Paper id='addPaper'>
-                                <Grid item xs={12} id='addExpenseSection'>
-                                    <Button id='toggleExpenseBtn' onClick={expenseInputToggle}><p style={{ marginRight: 10 }}>Add Expense</p> <FaFolderPlus style={{ fontSize: 24 }} /> </Button>
-                                </Grid>
-                            </Paper>
-                            <Paper id='expenseInputPaper'>
-                                <Grid item xs={12} id='expenseInputSection'>
-                                    {user ? <ExpenseForm user={user} setRetrieve={setRetrieve} /> : null}
-                                </Grid>
-                            </Paper>
-                        </Grid>
-                    </Grid>
-                    <Grid container id='contentContainer'>
-                        <Grid item xs={12} lg={5}>
-                            <Paper>
-                                {monthlyCharges && monthlyCharges.length > 0 ? <CHeader message={'Monthly'} total={totalMonthly} /> : <HelperText monthly={true} display={display} />}
-                                <Grid container className='expListCont'>
-                                    {monthlyCharges ? monthlyCharges.map((item, index) => <ChargeItem data={item} key={index} setRetrieve={setRetrieve} />) : null}
-                                </Grid>
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={12} lg={5}>
-                            <Paper>
-                                {oneTime && oneTime.length > 0 ? <CHeader message={'Non-Monthly'} /> : <HelperText monthly={false} display={display} />}
-                                <Grid container className='expListCont'>
-                                    {oneTime !== null ? oneTime.map((item, index) => <ChargeItem data={item} key={index} setRetrieve={setRetrieve} />) : null}
-                                </Grid>
-                            </Paper>
-                        </Grid>
-                    </Grid>
-                </>
-            }
+          <Grid container id='addCont'>
+            <Grid item xs={12} id='addItem'>
+              <Paper id='addPaper'>
+                <Grid item xs={12} id='addExpenseSection'>
+                  <Button id='toggleExpenseBtn' onClick={expenseInputToggle}>
+                    <p style={{ marginRight: 10 }}>Add Expense</p>{' '}
+                    <FaFolderPlus style={{ fontSize: 24 }} />{' '}
+                  </Button>
+                </Grid>
+              </Paper>
+              <Paper id='expenseInputPaper'>
+                <Grid item xs={12} id='expenseInputSection'>
+                  {user ? (
+                    <ExpenseForm
+                      user={user}
+                      update={() => retrieveContent()}
+                      setRetrieve={setRetrieve}
+                    />
+                  ) : null}
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid container id='homeContentGrid'>
+            <Overview user={user} />
+          </Grid>
         </>
-    )
-}
+      ) : (
+        <>
+          <Grid container id='addCont'>
+            <Grid item xs={12} id='addItem'>
+              <Paper id='addPaper'>
+                <Grid item xs={12} id='addExpenseSection'>
+                  <Button id='toggleExpenseBtn' onClick={expenseInputToggle}>
+                    <p style={{ marginRight: 10 }}>Add Expense</p>{' '}
+                    <FaFolderPlus style={{ fontSize: 24 }} />{' '}
+                  </Button>
+                </Grid>
+              </Paper>
+              <Paper id='expenseInputPaper'>
+                <Grid item xs={12} id='expenseInputSection'>
+                  {user ? (
+                    <ExpenseForm user={user} setRetrieve={setRetrieve} />
+                  ) : null}
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid container id='contentContainer'>
+            <Grid item xs={12} lg={5}>
+              <Paper>
+                {monthlyCharges && monthlyCharges.length > 0 ? (
+                  <CHeader message={'Monthly'} total={totalMonthly} />
+                ) : (
+                  <HelperText monthly={true} display={display} />
+                )}
+                <Grid container className='expListCont'>
+                  {monthlyCharges
+                    ? monthlyCharges.map((item, index) => (
+                        <ChargeItem
+                          data={item}
+                          key={index}
+                          setRetrieve={setRetrieve}
+                        />
+                      ))
+                    : null}
+                </Grid>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} lg={5}>
+              <Paper>
+                {oneTime && oneTime.length > 0 ? (
+                  <CHeader message={'Non-Monthly'} />
+                ) : (
+                  <HelperText monthly={false} display={display} />
+                )}
+                <Grid container className='expListCont'>
+                  {oneTime !== null && oneTime.length > 0
+                    ? oneTime.map((item, index) => (
+                        <ChargeItem
+                          data={item}
+                          key={index}
+                          setRetrieve={setRetrieve}
+                        />
+                      ))
+                    : null}
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+        </>
+      )}
+    </>
+  );
+};
 
 export default Content;
--- client/src/components/Content/HomeOverview/HomeOverview.jsx
+++ client/src/components/Content/HomeOverview/HomeOverview.jsx
@@ -5,155 +5,155 @@ import API from '../../../clientRoutes/API';
 import { Paper, Grid } from '@material-ui/core';
 
 const Overview = ({ user }) => {
+  const [profile, setProfile] = useState(null);
+  const [doughnut, setDoughnut] = useState(null);
+  const [nonMonDoughnut, setNonMonDoughnut] = useState(null);
 
-    const [profile, setProfile] = useState(null);
-    const [doughnut, setDoughnut] = useState(null);
-    const [nonMonDoughnut, setNonMonDoughnut] = useState(null);
+  useEffect(() => {
+    if (profile !== null) {
+      let financesTotal = 0;
+      let livingTotal = 0;
+      let healthTotal = 0;
+      let leisureTotal = 0;
+      let travelTotal = 0;
 
-    useEffect(() => {
-        if (profile !== null) {
-            let financesTotal = 0;
-            let livingTotal = 0;
-            let healthTotal = 0;
-            let leisureTotal = 0;
-            let travelTotal = 0;
+      let nonMonFin = 0;
+      let nonMonLiv = 0;
+      let nonMonHea = 0;
+      let nonMonLei = 0;
+      let nonMonTra = 0;
 
-            let nonMonFin = 0;
-            let nonMonLiv = 0;
-            let nonMonHea = 0;
-            let nonMonLei = 0;
-            let nonMonTra = 0;
-
-            profile.expense.map(item => {
-                switch (item.category) {
-                    case 'finances':
-                        if (item.monthly === true) return financesTotal += item.amount;
-                        else return nonMonFin += item.amount;
-                        break;
-                    case 'living':
-                        if (item.monthly === true) return livingTotal += item.amount;
-                        else return nonMonLiv += item.amount;
-                        break;
-                    case 'health':
-                        if (item.monthly === true) return healthTotal += item.amount;
-                        else return nonMonHea += item.amount;
-                        break;
-                    case 'leisure':
-                        if (item.monthly === true) return leisureTotal += item.amount;
-                        else return nonMonLei += item.amount;
-                        break;
-                    case 'travel':
-                        if (item.monthly === true) return travelTotal += item.amount;
-                        else return nonMonTra += item.amount;
-                        break;
-                    default:
-                        return item;
-                }
-            })
-
-            let total = financesTotal + livingTotal + healthTotal + leisureTotal + travelTotal;
-            let nonMonTotal = nonMonFin + nonMonLiv + nonMonHea + nonMonLei + nonMonTra;
-
-            setDoughnut({
-                labels: ['Finances', 'Living', 'Health', 'Leisure', 'Travel'],
-                datasets: [
-                    {
-                        label: `Recurring Monthly Expenses - ${total.toLocaleString("en-US", {
-                            style: "currency",
-                            currency: "USD"
-                        })}`,
-                        backgroundColor: [
-                            '#5A4218',
-                            'green',
-                            'red',
-                            'gold',
-                            'aquamarine'
-                        ],
-                        data: [financesTotal, livingTotal, healthTotal, leisureTotal, travelTotal]
-                    }
-
-                ]
-            })
-
-            setNonMonDoughnut({
-                labels: ['Finances', 'Living', 'Health', 'Leisure', 'Travel'],
-                datasets: [
-                    {
-                        label: `Msc. Charges - ${nonMonTotal.toLocaleString("en-US", {
-                            style: "currency",
-                            currency: "USD"
-                        })}`,
-                        backgroundColor: [
-                            '#5A4218',
-                            'green',
-                            'red',
-                            'gold',
-                            'aquamarine'
-                        ],
-                        data: [nonMonFin, nonMonLiv, nonMonHea, nonMonLei, nonMonTra]
-                    }
-
-                ]
-            })
-
+      profile.expense.map(item => {
+        switch (item.category) {
+          case 'finances':
+            if (item.monthly === true) return (financesTotal += item.amount);
+            else return (nonMonFin += item.amount);
+            break;
+          case 'living':
+            if (item.monthly === true) return (livingTotal += item.amount);
+            else return (nonMonLiv += item.amount);
+            break;
+          case 'health':
+            if (item.monthly === true) return (healthTotal += item.amount);
+            else return (nonMonHea += item.amount);
+            break;
+          case 'leisure':
+            if (item.monthly === true) return (leisureTotal += item.amount);
+            else return (nonMonLei += item.amount);
+            break;
+          case 'travel':
+            if (item.monthly === true) return (travelTotal += item.amount);
+            else return (nonMonTra += item.amount);
+            break;
+          default:
+            return item;
         }
-    }, [profile])
+      });
 
-    useEffect(() => {
-        API.getHomeDisplay({ id: user })
-            .then(response => { setProfile(response.data); })
-    }, [])
-    return (
-        <>
-            <Grid item xs={12} sm={12} md={6} lg={6} className='pieGridItem'>
-                {doughnut !== null ?
-                    <Paper id='recurringPiePaper'>
-                        <Doughnut id='recurringPie'
-                            data={doughnut}
-                            options={{
-                                title: {
-                                    display: true,
-                                    text: doughnut.datasets[0].label,
-                                    fontSize: 12
-                                },
-                                legend: {
-                                    display: true,
-                                    position: 'bottom',
-                                    maintainAspectRation: false
-                                }
-                            }}
-                        />
-                    </Paper>
-                    : <p>Loading</p>}
-            </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={6} className='pieGridItem'>
-                {nonMonDoughnut !== null ?
-                    <Paper id='recurringPiePaper'>
-                        <Doughnut id='recurringPie'
-                            data={nonMonDoughnut}
-                            options={{
-                                title: {
-                                    display: true,
-                                    text: nonMonDoughnut.datasets[0].label,
-                                    fontSize: 12
-                                },
-                                legend: {
-                                    display: true,
-                                    position: 'bottom',
-                                    maintainAspectRation: false
-                                }
-                            }}
-                        />
-                    </Paper>
-                    : <p>Loading</p>}
-            </Grid>
-            <Grid container>
-                <Paper>
-                    Some stuff here
-                </Paper>
-            </Grid>
-        </>
-    )
-}
+      let total =
+        financesTotal + livingTotal + healthTotal + leisureTotal + travelTotal;
+      let nonMonTotal =
+        nonMonFin + nonMonLiv + nonMonHea + nonMonLei + nonMonTra;
+
+      setDoughnut({
+        labels: ['Finances', 'Living', 'Health', 'Leisure', 'Travel'],
+        datasets: [
+          {
+            label: `Recurring Monthly Expenses - ${total.toLocaleString(
+              'en-US',
+              {
+                style: 'currency',
+                currency: 'USD'
+              }
+            )}`,
+            backgroundColor: ['#5A4218', 'green', 'red', 'gold', 'aquamarine'],
+            data: [
+              financesTotal,
+              livingTotal,
+              healthTotal,
+              leisureTotal,
+              travelTotal
+            ]
+          }
+        ]
+      });
+
+      setNonMonDoughnut({
+        labels: ['Finances', 'Living', 'Health', 'Leisure', 'Travel'],
+        datasets: [
+          {
+            label: `Msc. Charges - ${nonMonTotal.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            })}`,
+            backgroundColor: ['#5A4218', 'green', 'red', 'gold', 'aquamarine'],
+            data: [nonMonFin, nonMonLiv, nonMonHea, nonMonLei, nonMonTra]
+          }
+        ]
+      });
+    }
+  }, [profile]);
+
+  useEffect(() => {
+    API.getHomeDisplay({ id: user }).then(response => {
+      setProfile(response.data);
+    });
+  }, []);
+  return (
+    <>
+      <Grid item xs={12} sm={12} md={6} lg={6} className='pieGridItem'>
+        {doughnut !== null ? (
+          <Paper id='recurringPiePaper'>
+            <Doughnut
+              id='recurringPie'
+              data={doughnut}
+              options={{
+                title: {
+                  display: true,
+                  text: doughnut.datasets[0].label,
+                  fontSize: 12
+                },
+                legend: {
+                  display: true,
+                  position: 'bottom',
+                  maintainAspectRation: false
+                }
+              }}
+            />
+          </Paper>
+        ) : (
+          <p>Loading</p>
+        )}
+      </Grid>
+      <Grid item xs={12} sm={12} md={6} lg={6} className='pieGridItem'>
+        {nonMonDoughnut !== null ? (
+          <Paper id='recurringPiePaper'>
+            <Doughnut
+              id='recurringPie'
+              data={nonMonDoughnut}
+              options={{
+                title: {
+                  display: true,
+                  text: nonMonDoughnut.datasets[0].label,
+                  fontSize: 12
+                },
+                legend: {
+                  display: true,
+                  position: 'bottom',
+                  maintainAspectRation: false
+                }
+              }}
+            />
+          </Paper>
+        ) : (
+          <p>Loading</p>
+        )}
+      </Grid>
+      <Grid container id='mobileOverview'>
+        <Paper>Some stuff here</Paper>
+      </Grid>
+    </>
+  );
+};
 
 export default Overview;
