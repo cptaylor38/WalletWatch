4ce6822b9e573a25f4446b2af6c1a763e14592bc
Corbin Taylor <cptaylor38@yahoo.com>
2020-01-19 03:52:19 -0500

    d

---
+++ client/build/precache-manifest.94c7e21efeab5905be6877e5c67fe99d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14610a46def7617a106cb885e44604d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f8781d7bbd75178b827",
+    "url": "/static/css/main.fd70ed1c.chunk.css"
+  },
+  {
+    "revision": "c6a91d9a164070020983",
+    "url": "/static/js/2.818ea7ce.chunk.js"
+  },
+  {
+    "revision": "3f8781d7bbd75178b827",
+    "url": "/static/js/main.dbe2d253.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "0e99500fe6b0ca53f974079b35405ad8",
+    "url": "/static/media/globe.0e99500f.mp4"
+  },
+  {
+    "revision": "145a8ac23fea3ef8a9e95ac8048bfd0d",
+    "url": "/static/media/ogglobe.145a8ac2.ogv"
+  },
+  {
+    "revision": "128c48c67b305e3cc57aa7f3bbe97e7c",
+    "url": "/static/media/penny.128c48c6.svg"
+  },
+  {
+    "revision": "cf9b0c18a569dcd17a5d3fafcabdc66f",
+    "url": "/static/media/webmglobe.cf9b0c18.webm"
+  }
+]);
