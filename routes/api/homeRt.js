65d3c8510b927afa09410a2822ae4ea33227bb17
Corbin Taylor <cptaylor38@yahoo.com>
2020-01-19 16:19:09 -0500

    d

--- routes/api/homeRt.js
+++ routes/api/homeRt.js
@@ -1,13 +1,15 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const db = require('../../models');
 
 router.route('/:id').get((req, res) => {
-    db.User.findOne({ _id: req.params.id })
-        .populate('expense')
-        .exec((err, profile) => {
-            if (err) throw err;
-            res.json(profile);
-        })
-})
+  console.log(req.params.id);
+  db.User.findOne({ _id: req.params.id })
+    .populate('expense')
+    .exec((err, profile) => {
+      console.log(profile);
+      if (err) throw err;
+      res.json(profile);
+    });
+});
 
 module.exports = router;
