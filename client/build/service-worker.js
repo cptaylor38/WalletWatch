dd83daaa7cf2e3143eafc6ed7a565de03c2c761d
Corbin Taylor <cptaylor38@yahoo.com>
2020-01-19 21:24:12 -0500

    d

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a6d977334ee5f0913cb456abc236649.js"
+  "/precache-manifest.c35de7ea49185ee3dace6b697ea3ff8c.js"
 );
 
 self.addEventListener('message', (event) => {
